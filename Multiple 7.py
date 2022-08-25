c463b3cfedac0d239eda2a680062196ba9a694a6
Mahya-kamelan <110662014+Mahya-kamelan@users.noreply.github.com>
2022-08-26 00:02:21 +0430

    Update Multiple 7.py

--- Multiple 7.py
+++ Multiple 7.py
@@ -5,4 +5,4 @@ if (new_number == 0):
     print('Your number is divisible by 7.')
 elif (1 <= new_number <= 6):
     next_number = ((number - new_number ) + 7)
-    print("Your number is not divisible by 7, next number is " ,next_number)
+    print("Your number is not divisible by 7" ,next_number)
