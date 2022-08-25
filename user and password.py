c546a016da2eb044d4f2571dc7cd989cec82f267
Mahya-kamelan <110662014+Mahya-kamelan@users.noreply.github.com>
2022-08-25 23:50:04 +0430

    Update user and password.py

--- user and password.py
+++ user and password.py
@@ -1,14 +1,14 @@
-name = 'mahya'
-astro = '987654'
+Username = 'mahya'
+Pass = '751275'
 count = 0
 
 for i in range (3):
     user = input('Enter your user: ')
     password = input('Enter your password: ')
-    if (user == name) and (password == astro):
+    if (user == Username) and (password == pass):
         print('welcome!')
         break
-    elif (user != name) or (password != astro):
+    elif (user != Username) or (password != Pass):
         if (count == 2):
             print('Your account has been locked :(')
             break
