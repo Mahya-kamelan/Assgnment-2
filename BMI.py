dfb4056067106c328098b1efee911599ae73b4ca
Mahya-kamelan <110662014+Mahya-kamelan@users.noreply.github.com>
2022-08-26 00:01:11 +0430

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,18 +1,18 @@
-weight = float(input('Enter your weight in Kilograms: '))
-height = float(input('Enter your height in meters: '))
+weight = float(input('Enter your weight based on Kilograms: '))
+height = float(input('Enter your height based on meters: '))
 
 new_height = height * height
 Bmi = weight / new_height
 print(Bmi)
 if (16 < Bmi < 18.5):
-    print('You are body weight deficit, Be sure to check up on your body and diet.')
+    print('Your body weight is deficit,.')
 elif (18.5 <= Bmi < 24):
-    print('You are normal, keep going :)')
+    print('You are ok')
 elif (24 <= Bmi < 30):
-    print('You are weight over, You can reach a normal weight by eating an apple a day.')
+    print('Your weight is over.')
 elif (30 <= Bmi < 35):
-    print('You are obesity first degree, Eat less oil and carbohydrates.')
+    print('Youre obesity first degree.')
 elif ( 35 <= Bmi < 40):
-    print('You are obesity second degree, Use the elevator more so that your knees do not hurt.')
+    print('Youre obesity second degree')
 elif ( Bmi >= 40):
-    print('You are obesity third weight, I do not have anything to say to you because you have not taken any steps to reduce your weight so far, fat man :(')
+    print('Youre obesity third weight, Dude!')
