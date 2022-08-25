cff514fca5a6563cdb866369baa7c7093488eb65
Mahya-kamelan <110662014+Mahya-kamelan@users.noreply.github.com>
2022-08-25 23:56:08 +0430

    Update revers number.py

--- revers number.py
+++ revers number.py
@@ -1,19 +1,19 @@
-new_number = number= int(input('Enter your number: '))
+number = old_number= int(input('Enter your number: '))
 while True:
     if number < 0:
-        print('Please enter a positive and integer number.')
-        new_number = number= int(input('Enter your number: '))
+        print('Please give me a positive number.')
+        number = old_number= int(input('Enter your number: '))
     else:
         break
 
 rev = 0
-while new_number != 0:
-    if new_number >= 0:
-        x = new_number % 10
+while number != 0:
+    if number >= 0:
+        x = number % 10
         rev  = (rev*10) + x
-        new_number = int(new_number / 10)
+        number = int(number / 10)
 print(rev)
-if (rev == number):
-    print('The number is equal to its inverse.')
-elif (rev != number):
-    print('The number is not equal to its inverse.')
+if (rev == old_number):
+    print('The number is equal.')
+elif (rev != old_number):
+    print('The number is not equal.')
